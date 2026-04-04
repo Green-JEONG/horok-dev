35367e9a27f8277b82350e28f8c3ce983b4872a7
Horok <th2gr22n@gmail.com>
2026-04-04 18:47:13 +0900

    fix: sitemap baseurl 수정

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "localhost:3000";
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://horok.co.kr";
 
   return [
     {
